d48a4f2e7f4109f199a408fa9e88a3d09d687982
SarkarSoumitro <soumitrosarkar78@gmail.com>
2024-11-09 00:54:42 +0600

    updated

---
+++ doctor_appointment_ui/lib/Screen/DoctorDetailScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DoctorDetailScreen extends StatefulWidget {
+  const DoctorDetailScreen({super.key});
+
+  @override
+  State<DoctorDetailScreen> createState() => _DoctorDetailScreenState();
+}
+
+class _DoctorDetailScreenState extends State<DoctorDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Doctors details"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
---
+++ doctor_appointment_ui/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:doctor_appointment_ui/Screen/DoctorDetailScreen.dart';
+import 'package:doctor_appointment_ui/Screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Mainpage(),
+    );
+  }
+}
+
+class Mainpage extends StatefulWidget {
+  const Mainpage({super.key});
+
+  @override
+  State<Mainpage> createState() => _MainpageState();
+}
+
+class _MainpageState extends State<Mainpage> {
+  int selectedItem = 0;
+  final List<Widget> pages = [
+    HomeScreen(),
+    DoctorDetailScreen(),
+    HomeScreen(),
+    HomeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.deepPurple,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedItem,
+        onTap: (value) {
+          setState(() {
+            selectedItem = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.schedule), label: 'Schedule'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),
+      body: pages[selectedItem],
+    );
+  }
+}
