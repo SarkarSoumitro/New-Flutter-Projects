334bd5a6df08b0ae62e21a3aede3d23e545751b1
SarkarSoumitro <soumitrosarkar78@gmail.com>
2025-03-17 23:46:25 +0600

    b

---
+++ getx_practice/lib/alls/prac1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Practice1 extends StatelessWidget {
+  const Practice1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
