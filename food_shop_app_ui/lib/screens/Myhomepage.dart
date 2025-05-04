cc46b4e2b0962e19cfee95e42ed3be6bbd9c52f8
SarkarSoumitro <soumitrosarkar78@gmail.com>
2025-05-04 23:03:55 +0600

    fruits shop ui p1

---
+++ food_shop_app_ui/lib/screens/Myhomepage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:food_shop_app_ui/model/Model.dart';
+
+class Myhomepage extends StatefulWidget {
+  const Myhomepage({super.key});
+
+  @override
+  State<Myhomepage> createState() => _MyhomepageState();
+}
+
+class _MyhomepageState extends State<Myhomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * .05),
+        child: Column(
+          children: [
+            Myappbar(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .008,
+            ),
+            Text(
+              "Fruits and berries",
+              style: TextStyle(
+                  fontSize: 33,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .008,
+            ),
+            searchbar(),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: MasonryGridView.builder(
+                    crossAxisSpacing: 20,
+                    itemCount: foodShopItems().length,
+                    gridDelegate:
+                        SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      final item = foodShopItems()[index];
+                      return Padding(
+                        padding: EdgeInsets.only(bottom: 20),
+                        child: GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.only(top: 15, left: 20),
+                            height: item.height.toDouble(),
+                            decoration: BoxDecoration(
+                                color: item.color,
+                                borderRadius: BorderRadius.circular(30)),
+                          ),
+                        ),
+                      );
+                    }))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class searchbar extends StatelessWidget {
+  const searchbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          hintText: "Search",
+          hintStyle: TextStyle(fontSize: 16, color: Colors.black26),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none),
+          filled: true,
+          fillColor: Colors.grey[100],
+          contentPadding: EdgeInsets.all(20),
+          prefixIcon: Padding(
+            padding: EdgeInsets.only(left: 25, right: 15),
+            child: Icon(
+              Icons.search,
+              size: 30,
+            ),
+          )),
+    );
+  }
+}
+
+class Myappbar extends StatelessWidget {
+  const Myappbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.05),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 55,
+              width: 50,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(15)),
+              child: Icon(
+                Icons.keyboard_arrow_left,
+                size: 22,
+              ),
+            ),
+            Image.asset(
+              "assets/two line.png",
+              height: 40,
+              width: 40,
+            )
+          ],
+        ));
+  }
+}
---
+++ list_scroll_animation/lib/models/scrol_list_model.dart
@@ -0,0 +1,74 @@
+class ScrollItems {
+  final String image;
+  final String name;
+  final int color;
+
+  ScrollItems({
+    required this.image,
+    required this.name,
+    required this.color,
+  });
+}
+
+final dataItems = <ScrollItems>[
+  ScrollItems(
+    name: "Chicken",
+    image: "assets/chiken.jpg",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "Burger",
+    image: "assets/chiken.jpg",
+    color: 0xFF99C5FD,
+  ),
+  ScrollItems(
+    name: "Noodles",
+    image: "assets/chiken.jpg",
+    color: 0xFF40AC9C,
+  ),
+  ScrollItems(
+    name: "Lemon",
+    image: "assets/chiken.jpg",
+    color: 0xFFD6F670,
+  ),
+  ScrollItems(
+    name: "Rum",
+    image: "assets/chiken.jpg",
+    color: 0xFFE7668E,
+  ),
+  ScrollItems(
+    name: "Cheese",
+    image: "assets/chiken.jpg",
+    color: 0xFF99C5FD,
+  ),
+  ScrollItems(
+    name: "CocaCola",
+    image: "assets/chiken.jpg",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "Ice Cream",
+    image: "assets/chiken.jpg",
+    color: 0xFF447C12,
+  ),
+  ScrollItems(
+    name: "Pizza",
+    image: "assets/chiken.jpg",
+    color: 0xFFB1D1FF,
+  ),
+  ScrollItems(
+    name: "Chicken",
+    image: "assets/chiken.jpg",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "Burger",
+    image: "assets/chiken.jpg",
+    color: 0xFF99C5FD,
+  ),
+  ScrollItems(
+    name: "Noodles",
+    image: "assets/chiken.jpg",
+    color: 0xFF40AC9C,
+  ),
+];
